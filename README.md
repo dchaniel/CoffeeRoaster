80a0bfa547e4afe105f23b486f82ddcb5eb4b582
Daniel Chan <danielmchan1@gmail.com>
2024-05-04 15:59:47 -0700

    ci: integrate ruff linting and reformat code in GitHub Actions
    - Add ruff lint check to ensure code quality.
    - Reformat existing codebase using ruff for consistent style.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Coffee Roaster
 
 Code to control a coffee roaster using a Raspberry Pi, MCP9601 thermocouple amplifier, and a solid state relay.
+
+# TODO
+
+- [ ] Make the temperature profile come from a config file
+- [ ] Integrate a second thermocouple and try to control based also on ([Rate of Rise](https://www.roestcoffee.com/roestblog/rate-of-rise#:~:text=Rate%20of%20Rise%20(RoR)%20measures,heat%20from%20the%20roasting%20machine.))
+- [ ] Publish data to a server and create a webpage to display it. Host somewhere.
+- [ ] Make webpage take a profile and send it to the roaster to run
+- [ ] Save the data somewhere where we can see it online
--- main.py
+++ main.py
@@ -20,8 +20,9 @@ shared_state = {
     "current_time": 0.0,
     "current_temperature": 0.0,
     "ideal_temperature": 0.0,
-    "heater_status": "OFF"
+    "heater_status": "OFF",
 }
+
 
 def linear_interpolation(x, x_points, y_points):
     """Performs linear interpolation for a given x value."""
@@ -32,25 +33,34 @@ def linear_interpolation(x, x_points, y_points):
             return y0 + (y1 - y0) * ((x - x0) / (x1 - x0))
     return y_points[-1]  # Return the last temperature if x is beyond the profile
 
+
 async def update_temperature():
     """Updates the current temperature in the shared state every 0.1 seconds."""
     while True:
         shared_state["current_temperature"] = mcp.temperature
         await asyncio.sleep(0.1)
 
+
 async def control_loop():
     """Controls the heating element and updates the shared state."""
     start_time = time.time()
     times, temperatures = zip(*temperature_profile)
-    on = True
     while True:
         shared_state["current_time"] = time.time() - start_time
-        shared_state["ideal_temperature"] = linear_interpolation(shared_state["current_time"], times, temperatures)
+        shared_state["ideal_temperature"] = linear_interpolation(
+            shared_state["current_time"], times, temperatures
+        )
 
-        if shared_state["current_temperature"] < shared_state["ideal_temperature"] - deadband:
+        if (
+            shared_state["current_temperature"]
+            < shared_state["ideal_temperature"] - deadband
+        ):
             shared_state["heater_status"] = "ON"
             psm.value = True
-        elif shared_state["current_temperature"] > shared_state["ideal_temperature"] + deadband:
+        elif (
+            shared_state["current_temperature"]
+            > shared_state["ideal_temperature"] + deadband
+        ):
             shared_state["heater_status"] = "OFF"
             psm.value = False
         # psm.value = on
@@ -59,21 +69,29 @@ async def control_loop():
         # print(on)
         await asyncio.sleep(1)  # Control adjustments at 1Hz
 
+
 def get_timestamp_filename():
     """Generates a filename based on the current date and time."""
     t = time.localtime()  # Get the current time structure
     # Format the filename as "YYYY-MM-DD_HH-MM-SS.csv"
-    return "{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}.csv".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
+    return "{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}.csv".format(
+        t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec
+    )
+
 
 async def log_data():
     while True:
-        current_time = float(shared_state["current_time"])
         current_temperature = float(shared_state["current_temperature"])
         ideal_temperature = float(shared_state["ideal_temperature"])
         heater_status = 1 if psm.value else 0  # Convert boolean status to 1 or 0
-        print("{:.2f}, {:.2f}, {}".format(current_temperature, ideal_temperature, heater_status))
+        print(
+            "{:.2f}, {:.2f}, {}".format(
+                current_temperature, ideal_temperature, heater_status
+            )
+        )
         await asyncio.sleep(0.2)  # Logging at 10Hz
-        
+
+
 async def main():
     """Initializes and runs the update_temperature, control loop, and logging tasks."""
     temperature_task = asyncio.create_task(update_temperature())
@@ -81,4 +99,7 @@ async def main():
     logging_task = asyncio.create_task(log_data())
     await asyncio.gather(temperature_task, control_task, logging_task)
 
-asyncio.run(main())  # Uncomment this line if running in an environment that supports asyncio.run
+
+asyncio.run(
+    main()
+)  # Uncomment this line if running in an environment that supports asyncio.run
